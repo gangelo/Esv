bf338b6d4c8fa58a20dcdc3c628e428b75628d27
Gene M. Angelo, Jr <public.gma@gmail.com>
2011-07-02 20:07:10 -0400

    First commit of actual code

---
+++ EsvServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+
+
+namespace Esv {
+	internal class EsvServer {
+		static private object m_synchRoot = new object();
+
+		public EsvServer() {
+		}
+
+		static public bool GetReference(string reference, out string passage, out string errorMessage) {
+			lock (m_synchRoot) {
+				passage = string.Empty;
+				errorMessage = string.Empty;
+
+				StringBuilder urlBuilder = new StringBuilder();
+				urlBuilder.Append("http://www.esvapi.org/v2/rest/passageQuery");
+				urlBuilder.Append("?key=IP");
+				urlBuilder.Append("&passage=" + System.Uri.EscapeDataString(reference));
+				urlBuilder.Append("&output-format=plain-text");				
+
+				try {
+					try {
+						WebRequest webRequest = WebRequest.Create(urlBuilder.ToString());
+						using (StreamReader streamReader = new StreamReader(webRequest.GetResponse().GetResponseStream())) {
+							StringBuilder passageBuilder = new StringBuilder();
+							passageBuilder.Append(streamReader.ReadToEnd());
+							passage = passageBuilder.ToString();
+						}
+						return true;
+					} catch (Exception e) {
+						errorMessage = e.Message;
+						return false;
+					}
+				} finally {
+				}
+			}
+		}
+	};
+};
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Esv {
+	class Program {
+		static private string Prompt { get { return "Enter a passage to look up ('exit' to quit): "; } }
+
+		static void Main(string[] args) {
+
+			Console.Write(Program.Prompt);
+			string reference = Console.ReadLine();
+
+			while (!reference.Trim().Equals("exit", StringComparison.CurrentCultureIgnoreCase)) {
+				string passage;
+				string errorMessage;
+
+				if (!string.IsNullOrEmpty(reference)) {
+					if (Esv.EsvServer.GetReference(reference, out passage, out errorMessage)) {
+						Console.WriteLine(passage);
+					} else {
+						Console.WriteLine("An error occurred:");
+						Console.WriteLine(errorMessage);
+					}
+				}
+
+				Console.WriteLine();
+				Console.Write(Program.Prompt);
+				reference = Console.ReadLine();
+			}
+
+			//Console.WriteLine("Press any key to exit...");
+			//Console.ReadKey();
+		}
+	};
+};
